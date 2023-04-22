f7de7f1c043e3e5de5ad55fb78a1fc45fc23093f
tuyen10a6 <phamtuyenok2002@gmail.com>
2023-04-22 20:08:49 +0700

    GetAllProductApiSuccsessfully

---
+++ KFC_PhamTungDuong_User/Controllers/SanPhamController.cs
@@ -0,0 +1,23 @@
+﻿using BUS.IBus;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KFC_PhamTungDuong_User.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SanPhamController : ControllerBase
+    {
+        private ISanPhamUserBus _ProductBus;
+        public SanPhamController(ISanPhamUserBus productBus)
+        {
+            _ProductBus = productBus;
+        }
+        [HttpGet]
+        [Route("GetAllSanPham")]
+        public IActionResult GetAllSanPham()
+        {
+            return Ok(_ProductBus.GetAllSanPham());
+        }
+    }
+}
---
+++ Model/SanPhamModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODEL
+{
+    public class SanPhamModel
+    {
+        public int MaSanPham { get;set; }
+        public string TenSanPham { get; set; }
+        public int GiaTien { get; set; }
+        public string Image { get; set; }
+        public int MaDanhMuc { get; set; }
+        public int LuotXem { get; set; }
+        public string MoTa { get; set; }
+        public string MaNhaCungCap { get; set; }
+
+    }
+}
